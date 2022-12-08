6e9c1686785849a92be5a9a0263629439222dea0
destinr <destinroyer@gmail.com>
2022-12-07 20:33:09 -0800

    Landing page in progress.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,19 +3,30 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import AppLogo from '../assets/book.svg'
+import axios from 'axios'
+
+
+function logOut(){
+  axios
+  .post('/logout/')
+  .then((response) => {
+    console.log(response.data)
+    window.location.reload()
+  })
+}
 
 function AppNav(props) {
   return (
-    <Navbar fixed= 'top' fluid bg="light">
+    <Navbar fixed='top' fluid bg="light">
     <Container>
-    <Navbar.Brand href="#home">
+    <Navbar.Brand href="/">
             <img
               alt=""
               src={AppLogo}
               width="30"
               height="30"
               className="d-inline-block align-top"
-            />{' '}
+            />
             Plain meaning?
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -23,10 +34,13 @@ function AppNav(props) {
         <Nav className="me-auto">
           <Nav.Link href="/">Home</Nav.Link>
         </Nav>
-        <Nav className="ms-auto">
-          {!props.user && <Nav.Link  href="/#/auth">Log In/Sign Up</Nav.Link>}
-          {props.user && <Nav.Link  href="/#/user">User: {props.user.email}</Nav.Link>}
-        </Nav>
+        {props.user &&
+          <Nav className="ms-auto">
+            <Nav.Link  href="/#/user">User: {props.user.email}</Nav.Link> 
+            <Nav.Link  class='text-warning' to="/" onClick={()=>logOut()}><p class='text-danger'>Logout</p></Nav.Link> 
+          </Nav>
+        }
+        {!props.user && <Nav.Link  href="/#/auth">Log In/Sign Up</Nav.Link>}
       </Navbar.Collapse>
     </Container>
   </Navbar>
--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,36 +1,34 @@
 import axios from 'axios'
+import SCOTUS from '../assets/scotus.jpg'
+import {Button, Card} from "react-bootstrap";
+import ListGroup from 'react-bootstrap/ListGroup';
+import { Navigate, useNavigate } from "react-router-dom";
+
 
 function LandingPage(){
-    
-    function whoAmI(){
-        axios
-        .get('/currentuser')
-        .then((response) => {
-          console.log(response.data)
-        })
-      }
-    
-    function logOut(){
-        axios
-        .post('/logout/')
-        .then((response) => {
-          console.log(response.data)
-          window.location.reload()
-        })
-      }
+    let navigate = useNavigate()
+    function logIn(){
+      let path = '/auth'
+      navigate(path)
 
+    }
 
     return(
-        <div>
-            <button type="submit" className="btn btn-primary" onClick={()=>{whoAmI()}}>
-              Who am I?
-            </button>
-            <button type="submit" className="btn btn-primary" onClick={()=>{logOut()}}>
-              Logout
-            </button>
-        </div>
-
-    )
+      <body>
+        <Card>
+          <Card.Img src="https://www.heritage.org/sites/default/files/styles/facebook_optimized/public/images/2017-11/iStock-543822084.jpg?itok=EhPXKcdi" alt="Card image" />
+          <Card.ImgOverlay class='d-flex align-self-center'>
+            <Card.Title>Welcome to Plain Meaning?</Card.Title>
+            <Card.Text>
+              Textual legal analysis often relies on the "plain meaning" of statutes and codes. 
+              Unfortunately, “plain meanings” aren’t so plain; there are often a multitude of valid readings of a particular word or phrase.
+            </Card.Text>
+            <Card.Text>Plain Meaning? allows its users to submit their definition of oft-cited words to help provide an average "plain meaning.</Card.Text>
+            <Card.Link href="/#/auth">Log In/ Sign Up</Card.Link>
+          </Card.ImgOverlay>
+        </Card>
+    </body>
+  )
 }
 
 export default LandingPage
